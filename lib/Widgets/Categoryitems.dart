40867e6e6e852dabdfe66eaee9898d1850f39b7d
harikrish124 <harikrishnapradeep3@gmail.com>
2024-02-27 14:19:32 +0530

    HomePage UI

--- lib/Widgets/Categoryitems.dart
+++ lib/Widgets/Categoryitems.dart
@@ -8,7 +8,7 @@ class CategoryItems extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(right: 15, left: 15),
       child: Container(
-        padding: EdgeInsets.all(0),
+        padding: const EdgeInsets.all(0),
         color: const Color.fromRGBO(249, 249, 249, 100),
         margin: const EdgeInsets.only(
           bottom: 0,
