fca933499eab185beefdfc05987f442b4a250bfa
harikrish124 <harikrishnapradeep3@gmail.com>
2024-02-28 07:17:35 +0530

    CategoryPage Created

--- lib/BottamBar.dart
+++ lib/BottamBar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
-
-import 'package:wallymax/Screens/LikeScreen.dart';
+import 'package:wallymax/Screens/HomeScreen.dart';
 import 'package:wallymax/Screens/SettingsScreen.dart';
+import 'package:wallymax/Screens/categoryScreen.dart';
 
 class CustomnavBar extends StatelessWidget {
   const CustomnavBar({
@@ -14,26 +14,28 @@ class CustomnavBar extends StatelessWidget {
     return PersistentTabView(
       context,
       screens: [
-        const LikeScreen(),
-        Container(),
+        HomeScreen(),
+        const CategoryScreen(),
         const SettingScreen(),
       ],
       items: [
         PersistentBottomNavBarItem(
           inactiveColorPrimary: const Color.fromARGB(168, 168, 168, 100),
-          activeColorPrimary: Color.fromARGB(41, 2, 9, 53),
+          activeColorPrimary: const Color.fromARGB(244, 0, 0, 0),
           icon: const Icon(Icons.home),
           title: "Home",
         ),
         PersistentBottomNavBarItem(
           inactiveColorPrimary: const Color.fromARGB(168, 168, 168, 100),
-          icon: const Icon(Icons.search),
-          title: "Search",
+          activeColorPrimary: const Color.fromARGB(244, 0, 0, 0),
+          icon: const Icon(Icons.category_rounded),
+          title: "Category",
         ),
         PersistentBottomNavBarItem(
           inactiveColorPrimary: const Color.fromARGB(168, 168, 168, 100),
+          activeColorPrimary: const Color.fromARGB(244, 0, 0, 0),
           icon: const Icon(Icons.settings),
-          title: "settings",
+          title: "Setting",
         ),
       ],
       popAllScreensOnTapOfSelectedTab: false,
